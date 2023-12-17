63d38bd1918ab17ef2ef1678adeb6c417f3ab313
iindll <147333309+iindll@users.noreply.github.com>
2023-12-17 16:25:32 +0200

    X_O.cpp

---
+++ Board-Game/src/X_O.cpp
@@ -0,0 +1,141 @@
+GameManager::GameManager(Board* bPtr, Player* playerPtr[2] ) {
+    boardPtr = bPtr;
+    players[0] = playerPtr[0];
+    players[1] = playerPtr[1];
+}
+
+void GameManager::run() {
+    int x, y;
+    boardPtr->display_board();
+
+    while (!boardPtr->game_is_over()) {
+        for (int i : {0, 1}) {
+            players[i]->get_move(x, y);
+            while (!boardPtr->update_board(x, y, players[i]->get_symbol()) ) {
+
+                players[i]->get_move(x, y);
+            }
+            boardPtr->display_board();
+            if (boardPtr->is_winner()) {
+                cout << players[i]->to_string() << " wins\n";
+                return;
+            }
+            if (boardPtr->is_draw()) {
+                cout << "Draw!\n";
+                return;
+            }
+        }
+    }
+}
+
+RandomPlayer::RandomPlayer (char symbol, int dimension):Player(symbol)
+{
+    this->dimension = dimension;
+    this->name = "Random Computer Player";
+    cout << "My names is " << name << endl;
+}
+
+// Generate a random move
+void RandomPlayer::get_move (int& x, int& y) {
+    x = (int) (rand()/(RAND_MAX + 1.0) * dimension);
+    y = (int) (rand()/(RAND_MAX + 1.0) * dimension);
+}Player::Player(char symbol) {
+    this->symbol = symbol;
+}
+
+// Optionally, you can give him ID or order
+// Like Player 1 and Player 2
+Player::Player (int order, char symbol) {
+    cout << "Welcome player " << order << endl;
+    cout << "Please enter your name: ";
+    cin >> name;
+    this->symbol = symbol;
+}
+
+// Get desired move: x y (each between 0 and 2)
+// Virtual (can change for other player types)
+void Player::get_move (int& x, int& y) {
+    cout << "\nPlease enter your move x and y (0 to 4) separated by spaces: ";
+    cin >> x >> y;
+}
+
+
+// Give player info as a string
+string Player::to_string(){
+    return "Player: " + name ;
+}
+
+// Get symbol used by player
+char Player::get_symbol() {
+    return symbol;
+}
+
+// Set the board
+X_O_Board::X_O_Board () {
+    n_rows = n_cols = 3;
+    board = new char*[n_rows];
+    for (int i = 0; i < n_rows; i++) {
+        board [i] = new char[n_cols];
+        for (int j = 0; j < n_cols; j++)
+            board[i][j] = 0;
+    }
+}
+
+// Return true  if move is valid and put it on board
+// within board boundaries in empty cell
+// Return false otherwise
+bool X_O_Board::update_board (int x, int y, char mark){
+    // Only update if move is valid
+    if (!(x < 0 || x > 2 || y < 0 || y > 2) && (board[x][y] == 0)) {
+        board[x][y] = toupper(mark);
+        n_moves++;
+        return true;
+    }
+    else
+        return false;
+}
+
+// Display the board and the pieces on it
+void X_O_Board::display_board() {
+    for (int i: {0,1,2}) {
+        cout << "\n| ";
+        for (int j: {0,1,2}) {
+            cout << "(" << i << "," << j << ")";
+            cout << setw(2) << board [i][j] << " |";
+        }
+        cout << "\n-----------------------------";
+    }
+    cout << endl;
+}
+
+// Returns true if there is any winner
+// either X or O
+// Written in a complex way. DO NOT DO LIKE THIS.
+bool X_O_Board::is_winner() {
+    char row_win[3], col_win[3], diag_win[2];
+    for (int i:{0,1,2}) {
+        row_win[i] = board[i][0] & board[i][1] & board[i][2];
+        col_win[i] = board[0][i] & board[1][i] & board[2][i];
+    }
+    diag_win[0] = board[0][0] & board[1][1] & board[2][2];
+    diag_win[1] = board[2][0] & board[1][1] & board[0][2];
+
+    for (int i:{0,1,2}) {
+        if ( (row_win[i] && (row_win[i] == board[i][0])) ||
+             (col_win[i] && (col_win[i] == board[0][i])) )
+        {return true;}
+    }
+    if ((diag_win[0] && diag_win[0] == board[1][1]) ||
+        (diag_win[1] && diag_win[1] == board[1][1]))
+    {return true;}
+    return false;
+}
+
+// Return true if 9 moves are done and no winner
+bool X_O_Board::is_draw() {
+    return (n_moves == 9 && !is_winner());
+}
+
+bool X_O_Board::game_is_over () {
+    return n_moves >= 9;
+}
